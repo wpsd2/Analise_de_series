91c460f5b6373710dd53945bbe28d68ac8e3065b
Wanderson <wpsd2@hotmail.com>
2021-08-08 12:56:35 -0300

    update analise.py

--- analise_de_vendas.py
+++ analise_de_vendas.py
@@ -7,35 +7,35 @@ from pandas.plotting import autocorrelation_plot
 
 
 #bloco1
-car = pd.read_csv('arquivos\car.csv').head()
-print(f'Quantidade de linhas e colunas {car.shape}')
-car.head(10)
-print(f'Quantidade de dados nulos {car.isna().sum().sum()}')
-car.dtypes
-car['mes'] = pd.to_datetime(car['mes'])
+alucar = pd.read_csv('arquivos/alucar.csv').head()
+print(f'Quantidade de linhas e colunas {alucar.shape}')
+alucar.head(10)
+print(f'Quantidade de dados nulos {alucar.isna().sum().sum()}')
+alucar.dtypes
+alucar['mes'] = pd.to_datetime(alucar['mes'])
 
 #Gráfico1
-sns.lineplot(x='mes', y='vendas', data=car)
+sns.lineplot(x='mes', y='vendas', data=alucar)
 
 #Gráfico2
 sns.set_palette('Accent')
 sns.set_style('darkgrid')
-ax = sns.lineplot(x='mes', y='vendas', data=car)
+ax = sns.lineplot(x='mes', y='vendas', data=alucar)
 ax.figure.set_size_inches(12,6)
-ax.set_title('Vendas Car de 2017 e 2018', loc='left', fontsize=18)
+ax.set_title('Vendas alucar de 2017 e 2018', loc='left', fontsize=18)
 ax.set_xlabel('Tempo', fontsize=14)
 ax.set_ylabel('Vendas (R$)', fontsize=14)
 
 
 #Bloco2
-car['aumento'] = car[['vendas']].diff()
-car.head(8)
+alucar['aumento'] = alucar[['vendas']].diff()
+alucar.head(8)
 
 
 #Gráfico3
 sns.set_palette('Accent')
 sns.set_style('darkgrid')
-ax = sns.lineplot(x='mes', y='aumento', data=car)
+ax = sns.lineplot(x='mes', y='aumento', data=alucar)
 ax.figure.set_size_inches(12,6)
 ax.set_title('Aumento das vendas Car de 2017 e 2018', loc='left', fontsize=18)
 ax.set_xlabel('Tempo', fontsize=14)
@@ -54,22 +54,22 @@ def plotar(titulo, labelx, labely, x, y, dataset):
 
 
 #Gráfico4
-plotar('Aumento das vendas Car de 2017 e 2018', 'Tempo', 'Aumento', 'mes', 'aumento', car)
+plotar('Aumento das vendas Car de 2017 e 2018', 'Tempo', 'Aumento', 'mes', 'aumento', alucar)
 
 #Gráfico5
-car['aceleracao'] = car['aumento'].diff()
-plotar ('Aceleração das vendas da Car de 2017 e 2018', 'Tempo', 'Aceleração', 'mes','aceleracao', car)
+alucar['aceleracao'] = alucar['aumento'].diff()
+plotar ('Aceleração das vendas da alucar de 2017 e 2018', 'Tempo', 'Aceleração', 'mes','aceleracao', alucar)
 
 
 #Gráfico6
 plt.figure(figsize=(16,12))
 ax = plt.subplot(3,1,1)
 ax.set_title('Análise das vendas da Alucar 2017 e 2018',fontsize=18, loc='left')
-sns.lineplot(x='mes', y='vendas', data=car)
+sns.lineplot(x='mes', y='vendas', data=alucar)
 plt.subplot(3,1,2)
-sns.lineplot(x='mes', y='aumento', data=car)
+sns.lineplot(x='mes', y='aumento', data=alucar)
 plt.subplot(3,1,3)
-sns.lineplot(x='mes', y='aceleracao', data=car)
+sns.lineplot(x='mes', y='aceleracao', data=alucar)
 ax=ax
 
 
@@ -89,20 +89,20 @@ def plot_comparacao(x, y1, y2, y3, dataset, titulo):
 
 #Gráfico7
 plot_comparacao('mes', 'vendas', 'aumento', 'aceleracao',
-                car, 'Análise das vendas da Car de 2017 e 2018')
+                alucar, 'Análise das vendas da alucar de 2017 e 2018')
 
 
 
 #Gráfico8
 ax = plt.figure(figsize=(12,6))
-autocorrelation_plot(car['vendas'])
+autocorrelation_plot(alucar['vendas'])
 ax = ax
 
 
 #Gráfico9
 ax = plt.figure(figsize=(12,6))
 ax.suptitle('Correlação das Vendas', fontsize=18, x=0.26, y= 0.95)
-autocorrelation_plot(car['vendas'])
+autocorrelation_plot(alucar['vendas'])
 ax=ax
 
 
@@ -110,7 +110,7 @@ ax=ax
 #Gráfico10
 ax = plt.figure(figsize=(12,6))
 ax.suptitle('Correlação do aumento', fontsize=18, x=0.26, y= 0.95)
-autocorrelation_plot(car['aumento'][1:])
+autocorrelation_plot(alucar['aumento'][1:])
 ax=ax
 
 
@@ -118,5 +118,5 @@ ax=ax
 #Gráfico11
 ax = plt.figure(figsize=(12,6))
 ax.suptitle('Correlação da aceleraçao', fontsize=18, x=0.26, y= 0.95)
-autocorrelation_plot(car['aceleracao'][2:])
+autocorrelation_plot(alucar['aceleracao'][2:])
 ax=ax
