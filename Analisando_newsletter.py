cc395bfb3c670b551390295bffa5af4a9a4a92d1
Wanderson <wpsd2@hotmail.com>
2021-08-08 13:06:10 -0300

    new analise

---
+++ Analisando_newsletter.py
@@ -0,0 +1,33 @@
+#Analisando assinantes da newsletter
+
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from pandas.plotting import autocorrelation_plot
+
+
+
+
+assinantes = pd.read_csv('/content/newsletter_alucar.csv')
+assinantes.head()
+
+assinantes.dtypes
+
+
+print('Quantidade de linhas e colunas', assinantes.shape)
+print('Quantidade de dados nulos', assinantes.isna().sum().sum())
+
+
+assinantes['mes'] = pd.to_datetime(assinantes['mes'])
+assinantes.dtypes
+
+
+assinantes['aumento'] = assinantes ['assinantes'].diff()
+assinantes['aceleracao'] = assinantes ['aumento'].diff()
+assinantes.head()
+
+
+plot_comparacao('mes', 'assinantes', 'aumento', 'aceleracao',
+                assinantes, 'Análise de assinantes da newsletter')
+
+
