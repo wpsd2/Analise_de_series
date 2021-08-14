00cc34d8d1171b15f6834036823e3000671bfe29
Wanderson <wpsd2@hotmail.com>
2021-08-14 18:51:25 -0300

    media

--- alucel_analise_vendas.py
+++ alucel_analise_vendas.py
@@ -3,7 +3,7 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from pandas.plotting import autocorrelation_plot
 
-from analise_de_vendas import plot_comparacao
+from analise_de_vendas import plot_comparacao, plotar
 
 alucel = pd.read_csv('arquivos/alucel.csv')
 alucel.head()
@@ -22,4 +22,16 @@ alucel ['aceleracao'] = alucel ['aumento'].diff()
 plot_comparacao('dia', 'vendas', 'aumento', 'aceleracao', alucel, 'Análise de vendas da Alucel de Outubro e Novembro de 2018')
 
 
+#Média Móvel
+alucel['media_movel'] = alucel['vendas'].rolling(7).mean()
+#G1
+plotar('Análise de vendas com média móvel de 7 dias', 'Tempo', 'Média Móvel', 'dia', 'media_movel', alucel)
+
+alucel['media_movel_21'] = alucel['vendas'].rolling(21).mean()
+
+#G2
+plotar('Análise de vendas com média móvel de 21 dias', 'Tempo', 'Média Móvel', 'dia', 'media_movel_21', alucel)
+
+#G3
+plot_comparacao('dia', 'vendas', 'media_movel', 'media_movel_21', alucel, 'Comparando as médias móveis')
 
